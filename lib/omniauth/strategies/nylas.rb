6f3c305c3aad3e13bfe353a37130f7ed2309652d
Ozmar Ugarte <ozmar.ugarte@salesloft.com>
2022-09-22 14:51:34 -0500

    Add client_id and client_secret as token_options
    - In order to work with OmniAuth and nylas we need to send those
    params

--- lib/omniauth/strategies/nylas.rb
+++ lib/omniauth/strategies/nylas.rb
@@ -4,12 +4,14 @@ module OmniAuth
   module Strategies
     class Nylas < OmniAuth::Strategies::OAuth2
       option :name, "nylas"
-
       option :client_options, {
         :site          => "https://api.nylas.com",
         :authorize_url => "/oauth/authorize",
         :token_url     => "/oauth/token"
       }
+      option :client_id, ENV["NYLAS_APP_ID"]
+      option :client_secret, ENV["NYLAS_APP_SECRET"]
+      option :token_options, [:client_id, :client_secret]
 
       uid { access_token.params["account_id"] }
 
